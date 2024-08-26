801f73900e55d3aa7b5f40a2247aa056cf456909
RitaTeelaunch <rita@teelaunch.com>
2024-08-26 09:24:12 +0300

    webPackage

--- src/component/Test.tsx
+++ src/component/Test.tsx
@@ -10,30 +10,33 @@ const Test: React.FC  = () => {
     };
 
     return (
-        <div style={{display:"flex", flexDirection:"row",justifyContent:"space-between", marginBottom:"50px", gap:"50px"}}
-            // className={styles.container}
+        <div
+            // style={{display:"flex", flexDirection:"row",justifyContent:"space-between", marginBottom:"50px", gap:"50px"}}
+            className={styles.subContainer}
         >
             <p> Welcome </p>
-            <div style={{display:"flex", flexDirection:"row", backgroundColor:"red", justifyContent:"space-between", gap:"50p"}}
-                // className={styles.inputGroup}
+            <div
+                // style={{display:"flex", flexDirection:"row", backgroundColor:"red", justifyContent:"space-between", gap:"50px"}}
+                className={styles.inputGroup}
             >
                 <input
                     type="text"
                     placeholder="First Name"
                     value={firstName}
                     onChange={(e) => setFirstName(e.target.value)}
-                    // className={styles.input}
+                    className={styles.input}
                 />
                 <input
                     type="text"
                     placeholder="Last Name"
                     value={lastName}
                     onChange={(e) => setLastName(e.target.value)}
-                    // className={styles.input}
+                    className={styles.input}
                 />
             </div>
-            <button style={{display:"flex", paddingTop:"50px"}}
-                // className={`${styles.button} ${isClicked ? styles.buttonClicked : ''}`}
+            <button
+                // style={{display:"flex", paddingTop:"50px"}}
+                className={`${styles.button} ${isClicked ? styles.buttonClicked : ''}`}
                 onClick={handleButtonClick}
             >
                 {isClicked ? 'Clicked!' : 'Click Me'}
